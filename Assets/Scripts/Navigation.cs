5f3841c107c83a925339db329f2e5d1b90ca0d8f
lipyc <rodrigosamuel8@gmail.com>
2024-05-27 15:56:40 +0100

    testevilao

---
+++ Assets/Scripts/Navigation.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Navigation : MonoBehaviour
+{
+
+    public Transform palyer;
+    private NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = palyer.position;
+
+        agent.s
+    }
+
+    private void GoToTarget()
+    {
+        agent.
+    }
+}
