564812e4626616f612f1a32ff5e7d95c93bb6d8e
CarolNeves23947 <121882827+caroll2@users.noreply.github.com>
2024-05-27 16:59:36 +0100

    Reset Commits

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Animator playerAnim;
+	public Rigidbody playerRigid;
+	public float w_speed, wb_speed, olw_speed, rn_speed, ro_speed;
+	public bool walking;
+	public Transform playerTrans;
+	
+	
+	void FixedUpdate(){
+		if(Input.GetKey(KeyCode.W)){
+			playerRigid.velocity = transform.forward * w_speed * Time.deltaTime;
+		}
+		if(Input.GetKey(KeyCode.S)){
+			playerRigid.velocity = -transform.forward * wb_speed * Time.deltaTime;
+		}
+	}
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.W)){
+			playerAnim.SetTrigger("walk");
+			playerAnim.ResetTrigger("idle");
+			walking = true;
+			//steps1.SetActive(true);
+		}
+		if(Input.GetKeyUp(KeyCode.W)){
+			playerAnim.ResetTrigger("walk");
+			playerAnim.SetTrigger("idle");
+			walking = false;
+			//steps1.SetActive(false);
+		}
+		if(Input.GetKeyDown(KeyCode.S)){
+			playerAnim.SetTrigger("walk");
+			playerAnim.ResetTrigger("idle");
+			//steps1.SetActive(true);
+		}
+		if(Input.GetKeyUp(KeyCode.S)){
+			playerAnim.ResetTrigger("walk");
+			playerAnim.SetTrigger("idle");
+			//steps1.SetActive(false);
+		}
+		if(Input.GetKey(KeyCode.A)){
+			playerTrans.Rotate(0, -ro_speed * Time.deltaTime, 0);
+		}
+		if(Input.GetKey(KeyCode.D)){
+			playerTrans.Rotate(0, ro_speed * Time.deltaTime, 0);
+		}
+		if(walking == true){
+			if(Input.GetKeyDown(KeyCode.LeftShift)){
+				//steps1.SetActive(false);
+				//steps2.SetActive(true);
+				w_speed = w_speed + rn_speed;
+				playerAnim.SetTrigger("dash");
+				playerAnim.ResetTrigger("walk");
+			}
+			if(Input.GetKeyUp(KeyCode.LeftShift)){
+				//steps1.SetActive(true);
+				//steps2.SetActive(false);
+				w_speed = olw_speed;
+				playerAnim.ResetTrigger("dash");
+				playerAnim.SetTrigger("walk");
+			}
+		}
+	}
 }
