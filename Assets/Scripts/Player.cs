344080de928d9aabeeb325f6b552a02cf01b8b52
AiScream01 <ruicosta.der@gmail.com>
2024-06-02 18:19:40 +0100

    Main Character melhorias

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,64 +5,119 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     public Animator playerAnim;
-	public Rigidbody playerRigid;
-	public float w_speed, wb_speed, olw_speed, rn_speed, ro_speed;
-	public bool walking;
-	public Transform playerTrans;
-	
-	
-	void FixedUpdate(){
-		if(Input.GetKey(KeyCode.W)){
-			playerRigid.velocity = transform.forward * w_speed * Time.deltaTime;
-		}
-		if(Input.GetKey(KeyCode.S)){
-			playerRigid.velocity = -transform.forward * wb_speed * Time.deltaTime;
-		}
-	}
-	void Update(){
-		if(Input.GetKeyDown(KeyCode.W)){
-			playerAnim.SetTrigger("walk");
-			playerAnim.ResetTrigger("idle");
-			walking = true;
-			//steps1.SetActive(true);
-		}
-		if(Input.GetKeyUp(KeyCode.W)){
-			playerAnim.ResetTrigger("walk");
-			playerAnim.SetTrigger("idle");
-			walking = false;
-			//steps1.SetActive(false);
-		}
-		if(Input.GetKeyDown(KeyCode.S)){
-			playerAnim.SetTrigger("walk");
-			playerAnim.ResetTrigger("idle");
-			//steps1.SetActive(true);
-		}
-		if(Input.GetKeyUp(KeyCode.S)){
-			playerAnim.ResetTrigger("walk");
-			playerAnim.SetTrigger("idle");
-			//steps1.SetActive(false);
-		}
-		if(Input.GetKey(KeyCode.A)){
-			playerTrans.Rotate(0, -ro_speed * Time.deltaTime, 0);
-		}
-		if(Input.GetKey(KeyCode.D)){
-			playerTrans.Rotate(0, ro_speed * Time.deltaTime, 0);
-		}
-		if(walking == true){
-			if(Input.GetKeyDown(KeyCode.LeftShift)){
-				//steps1.SetActive(false);
-				//steps2.SetActive(true);
-				w_speed = w_speed + rn_speed;
-				playerAnim.SetTrigger("dash");
-				playerAnim.ResetTrigger("walk");
-			}
-			if(Input.GetKeyUp(KeyCode.LeftShift)){
-				//steps1.SetActive(true);
-				//steps2.SetActive(false);
-				w_speed = olw_speed;
-				playerAnim.ResetTrigger("dash");
-				playerAnim.SetTrigger("walk");
-			}
-		}
-	}
+    public Rigidbody rb;
+    public float w_speed, wb_speed, olw_speed, rn_speed, ro_speed, jumpForce, playerHeight;
+    public bool walking;
+    public Transform playerTrans;
+    private bool isGrounded;
+	public LayerMask whatIsGround;
+	bool readyToJump = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+		ResetJump();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = transform.forward * w_speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = -transform.forward * wb_speed * Time.deltaTime;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void Update()
+    {
+		rb.freezeRotation = true;
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            playerAnim.SetTrigger("walk");
+            playerAnim.ResetTrigger("idle");
+            walking = true;
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            playerAnim.ResetTrigger("walk");
+            playerAnim.SetTrigger("idle");
+            walking = false;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            playerAnim.SetTrigger("walk");
+            playerAnim.ResetTrigger("idle");
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            playerAnim.ResetTrigger("walk");
+            playerAnim.SetTrigger("idle");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerTrans.Rotate(0, -ro_speed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerTrans.Rotate(0, ro_speed * Time.deltaTime, 0);
+        }
+        if (walking)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                w_speed += rn_speed;
+                playerAnim.SetTrigger("dash");
+                playerAnim.ResetTrigger("walk");
+            }
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                w_speed = olw_speed;
+                playerAnim.ResetTrigger("dash");
+                playerAnim.SetTrigger("walk");
+            }
+        }
+
+		isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        // jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+			rb.velocity = new Vector3(rb.velocity.x, 10f, rb.velocity.z);
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            playerAnim.SetTrigger("jump");
+            isGrounded = false;
+			readyToJump = false;
+			//Jump();
+        }
+    }
+
+	//private void Jump()
+    //{
+    //    rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+    //    rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    //}
+//
+	private void ResetJump()
+    {
+        readyToJump = true;
+    }
 }
