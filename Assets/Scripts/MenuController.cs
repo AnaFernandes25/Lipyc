892b2ac556fc16d5788a6facca78d5bb8bf2e96b
Ana Figueiredo <beatriz1002figueiredo@gmail.com>
2024-06-15 21:20:27 +0100

    trabalho no menu

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,25 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 using UnityEngine.Video;
 
 public class MenuController : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
-    public GameObject menuOpcoes, rawImage;
-    public AudioSource selectSound;
-    public Animator animatorRawImage;
-    public Button ButtonPlay;
-
-
+    public GameObject menuOpcoes;
 
     // Start is called before the first frame update
     void Start()
     {
-        rawImage.SetActive(false);
-        animatorRawImage = rawImage.GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
@@ -27,16 +19,8 @@ public class MenuController : MonoBehaviour
     {
         if (!videoPlayer.isPlaying && Input.anyKeyDown)
         {
-            selectSound.Play();
             videoPlayer.Play();
-            rawImage.SetActive(true);
-            animatorRawImage.SetTrigger("fadein");
             menuOpcoes.SetActive(true);
         }
-
-    }
-    public void Play()
-    {
-        SceneManager.LoadScene("intro");
     }
 }
