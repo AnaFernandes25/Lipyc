80c94bc2a756b2498f26f53fe97c615cf65db741
Ana Figueiredo <beatriz1002figueiredo@gmail.com>
2024-06-17 05:22:54 +0100

    Configs

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Video;
 
 public class MenuController : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
-    public GameObject menuOpcoes, rawImage;
+    public GameObject menuInicial, menuOptions, rawImage;
     public AudioSource selectSound;
+    public string newGameScene;
     private Animator animatorRawImage;
 
     // Start is called before the first frame update
     void Start()
     {
         rawImage.SetActive(false);
+        menuOptions.SetActive(false);
         animatorRawImage = rawImage.GetComponent<Animator>();
     }
 
@@ -26,7 +29,41 @@ public class MenuController : MonoBehaviour
             videoPlayer.Play();
             rawImage.SetActive(true );
             animatorRawImage.SetTrigger("fadeIn");
-            menuOpcoes.SetActive(true);
+            menuInicial.SetActive(true);
         }
+    }
+
+    public void Options()
+    {
+        menuInicial.SetActive(false);
+        menuOptions.SetActive(true);
+
+    }
+
+    public void Salvar()
+    {
+        SaveConfigs();
+        ReturnMenuInicial();
+    }
+
+    public void ReturnMenuInicial()
+    {
+        menuInicial.SetActive(true);
+        menuOptions.SetActive(false);
+    }
+    
+    public void NewGame()
+    {
+        SceneManager.LoadScene(newGameScene);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    private void SaveConfigs()
+    {
+
     }
 }
